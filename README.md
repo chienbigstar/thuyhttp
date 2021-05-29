9b8a1f25b20352052c5a5b6f814b54c175b55a9e
Hieu Pham <hieupct98@gmail.com>
2021-05-29 17:13:19 +0700

    done

---
+++ README.md
@@ -0,0 +1,35 @@
+I. Cach dung:
+- yarn
+- yarn dev
+- port mac dinh: 3001
+
+II. List API:
+1. Get list truyen theo catId:
+- API: GET /api/v1/novel-by-catId/:catId
+- request:
++ catId lay tu path
++ offset: lay tu req.query, mac dinh la 0
++ limit: lay tu req.query, mac dinh la 10
+
+2. Get list truyen moi nhat:
+- API: GET /api/v1/newest-novel
+- request:
++ offset: lay tu req.query, mac dinh la 0
++ limit: lay tu req.query, mac dinh la 10
+
+3. Get list chapter theo truyen:
+- API: GET /api/v1/chapter-by-novelId/:novelId
+- request:
++ novelId lay tu path
++ offset: lay tu req.query, mac dinh la 0
++ limit: lay tu req.query, mac dinh la 10
+
+4. Get chapter:
+- API: GET /api/v1/chapter/:chapterId
+- request:
++ chapterId lay tu path
+
+5. Get truyen search by name:
+- API: GET /api/v1/novel-search-by-name
+- request:
++ search: lay tu req.query, mac dinh la ""
--- src/routes/index.js
+++ src/routes/index.js
@@ -16,12 +16,12 @@ router.get(`${apiPrefix}/novel-by-catId/:catId`, (req, res) => {
         "SELECT COUNT(*) FROM `novels` INNER JOIN `category_novel` on `novels`.`id` = `category_novel`.`novelId` INNER JOIN `categories` on `category_novel`.`categoryId` = `categories`.`id` WHERE `category_novel`.`categoryId` = ?";
 
     let totalCount;
-    db.all(countSql, [catId], (err, rows) => {
+    db.get(countSql, [catId], (err, rows) => {
         if (err) {
             res.status(400).json({ error: err.message });
         }
 
-        totalCount = rows[0]["COUNT(*)"];
+        totalCount = rows["COUNT(*)"];
 
         const sql =
             "SELECT novels.* FROM `novels` INNER JOIN `category_novel` on `novels`.`id` = `category_novel`.`novelId` INNER JOIN `categories` on `category_novel`.`categoryId` = `categories`.`id` WHERE `category_novel`.`categoryId` = ? ORDER BY updatedAt DESC LIMIT " +
@@ -29,11 +29,109 @@ router.get(`${apiPrefix}/novel-by-catId/:catId`, (req, res) => {
 
         db.all(sql, [catId], (err1, rows1) => {
             if (err1) {
-                console.log(err1);
                 return res.status(400).json({ error: err1.message });
             }
 
-            return res.status(200).json(rows1);
+            return res.status(200).json({
+                results: rows1,
+                totalCount: totalCount,
+            });
+        });
+    });
+});
+
+router.get(`${apiPrefix}/newest-novel`, (req, res) => {
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = parseInt(req.query.offset) || 0;
+
+    const sql = "SELECT * FROM `novels` ORDER BY updatedAt DESC LIMIT " + `${offset}, ${limit}`;
+
+    db.all(sql, (err, rows) => {
+        if (err) {
+            return res.status(400).json({ error: err.message });
+        }
+
+        return res.status(200).json({
+            results: rows,
+        });
+    });
+});
+
+router.get(`${apiPrefix}/chapter-by-novelId/:novelId`, (req, res) => {
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = parseInt(req.query.offset) || 0;
+    const novelId = parseInt(req.params.novelId);
+
+    const countSql = "SELECT COUNT(*) from `chapters` where `novelId` = ?";
+
+    let totalCount;
+    db.get(countSql, [novelId], (err, rows) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        }
+
+        totalCount = rows["COUNT(*)"];
+
+        const sql =
+            "SELECT `id`, `title`, `image`, `chapterNumber` from `chapters` where `novelId` = ? ORDER BY chapterNumber ASC LIMIT " +
+            `${offset}, ${limit}`;
+
+        db.all(sql, [novelId], (err1, rows1) => {
+            if (err1) {
+                return res.status(400).json({ error: err1.message });
+            }
+
+            return res.status(200).json({
+                results: rows1,
+                totalCount: totalCount,
+            });
+        });
+    });
+});
+
+router.get(`${apiPrefix}/chapter/:chapterId`, (req, res) => {
+    const chapterId = parseInt(req.params.chapterId);
+
+    const sql = "SELECT * FROM `chapters` WHERE `id` = ? LIMIT 1";
+
+    db.get(sql, [chapterId], (err, rows) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        }
+
+        return res.status(200).json(rows);
+    });
+});
+
+router.get(`${apiPrefix}/novel-search-by-name`, (req, res) => {
+    const limit = parseInt(req.query.limit) || 10;
+    const offset = parseInt(req.query.offset) || 0;
+    const search = req.query.search || "";
+    const searchValue = `%${search}%`;
+
+    const countSql = "SELECT COUNT(*) FROM `novels` WHERE `name` LIKE ? ORDER BY updatedAt DESC";
+
+    let totalCount;
+    db.get(countSql, [searchValue], (err, rows) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        }
+
+        totalCount = rows["COUNT(*)"];
+
+        const sql =
+            "SELECT * FROM `novels` WHERE `name` LIKE ? ORDER BY updatedAt DESC LIMIT " +
+            `${offset}, ${limit}`;
+
+        db.all(sql, [searchValue], (err1, rows1) => {
+            if (err1) {
+                return res.status(400).json({ error: err1.message });
+            }
+
+            return res.status(200).json({
+                results: rows1,
+                totalCount: totalCount,
+            });
         });
     });
 });
